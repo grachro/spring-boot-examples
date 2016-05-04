983f35570b1f180c18489c4f867ef4ca8dc3492f
grachro <grachro@gmail.com>
2016-05-04 11:19:52 +0900

    maven-java-web-jpa-h2 create from Spring Initializr

---
+++ maven-java-web-jpa-h2/src/main/java/com/grachro/springboot/MavenJavaWebJpaH2Application.java
@@ -0,0 +1,12 @@
+package com.grachro.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJavaWebJpaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenJavaWebJpaH2Application.class, args);
+	}
+}
